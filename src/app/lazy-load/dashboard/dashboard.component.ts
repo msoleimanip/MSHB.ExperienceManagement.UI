dbe30a11c7e1ed120011b98c65d0fa969b3366fb
hamedbagheri1992 <bagheri.h224@gmail.com.com>
2019-07-19 23:08:35 +0430

    init

--- src/app/lazy-load/dashboard/dashboard.component.ts
+++ src/app/lazy-load/dashboard/dashboard.component.ts
@@ -2,6 +2,8 @@ import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 import { environment } from './../../../environments/environment';
+import { User } from 'src/app/dataModels/viewModels/user';
+import { AuthenticationService } from 'src/app/core/authentication.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -14,8 +16,10 @@ export class DashboardComponent implements OnInit {
   hamedList = [1, 2, 3, 4, 5, 6, 7, 8];
 
   searchContent = '';
-
-  constructor(public translate: TranslateService, private router: Router) {
+  currentUser: User;
+  
+  constructor(private authenticationService: AuthenticationService,public translate: TranslateService, private router: Router) {
+    this.authenticationService.currentUser.subscribe(x => this.currentUser = x);
     translate.setDefaultLang(environment.language);
   }
 
